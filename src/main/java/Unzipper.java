05a5f7f0e57b123462443782a0355a04a6953e02
Orrin Rohrer <orrin.rohrer@gmail.com>
2023-11-08 16:09:16 -0600

    Update Unzipper.java

--- src/main/java/Unzipper.java
+++ src/main/java/Unzipper.java
@@ -8,7 +8,7 @@ public class Unzipper {
         butler.welcome();
         // https://app.snyk.io/vuln/SNYK-JAVA-ORGND4J-72550
         // should appear as Reachable
-        ArchiveUtils.unzipFileTo("./malicious_file.zip", "./unzipped/");
+        //ArchiveUtils.unzipFileTo("./malicious_file.zip", "./unzipped/");
         File f = new File("/tmp/evil.txt");
         if (f.exists()) {
             throw new Exception("Malicious file /tmp/evil.txt was created");
